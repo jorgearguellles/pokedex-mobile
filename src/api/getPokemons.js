517c7dcca57249d30d91a982bc1eb3ea3ba657b3
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-04-28 21:39:37 -0500

    [Add] getPokemonsByUrlAPI
      - [Add] logic to get pokemon by url
      - [Update] getPokemosFromAPI => getPokemosAPI

--- src/api/getPokemons.js
+++ src/api/getPokemons.js
@@ -1,12 +1,22 @@
 import { constants } from "../utils/constants";
 
-export async function getPokemons() {
+export async function getPokemonsAPI() {
   try {
     const url = `${constants.API_HOST}/pokemon?limit=20&offset=0`;
     const response = await fetch(url);
     const result = await response.json();
     return result;
   } catch (error) {
-    console.error(error);
+    throw error;
+  }
+}
+
+export async function getPokemonDetailsByUrlAPI(url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
   }
 }
--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,8 +1,11 @@
 import { Text, SafeAreaView } from "react-native";
 import React, { useState, useEffect } from "react";
-import { getPokemons } from "../api/getPokemons";
+import { getPokemonsAPI, getPokemonDetailsByUrlAPI } from "../api/getPokemons";
 
 export default function PokedexScreen() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log("Pokemons --->", pokemons);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -11,10 +14,24 @@ export default function PokedexScreen() {
 
   const loadPokemons = async () => {
     try {
-      const response = await getPokemons();
-      console.log(response);
+      const response = await getPokemonsAPI();
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlAPI(pokemon.url);
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
-      console.error("Error try load pokemons:", error);
+      console.error("Error trying load pokemons ==> ", error);
     }
   };
 
